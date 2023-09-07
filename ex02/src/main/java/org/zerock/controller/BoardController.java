dbafde710be746abb65a096b994f748f9bca18c4
KANG\b2000 <hanqjun2660@gmail.com>
2023-09-07 14:37:41 +0900

    post 형식의 controller 메서드 테스트 코드

--- ex02/src/main/java/org/zerock/controller/BoardController.java
+++ ex02/src/main/java/org/zerock/controller/BoardController.java
@@ -5,7 +5,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import org.zerock.domain.BoardVO;
 import org.zerock.service.BoardService;
 
 @Controller
@@ -24,5 +27,17 @@ public class BoardController {
     public void list(Model model) {
         log.info("list...........");
         model.addAttribute("list", boardService.getList());
+    }
+
+    @PostMapping("/register")
+    public String register(BoardVO boardVO, RedirectAttributes rttr) {
+
+        log.info("register....." + boardVO);
+
+        boardService.register(boardVO);
+
+        rttr.addAttribute("result", boardVO.getBno());
+
+        return "redirect:/board/list";
     }
 }
