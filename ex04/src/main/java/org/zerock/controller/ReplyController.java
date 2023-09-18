9d3495ac86ee64f9a85d6a6a4b58bdb683693386
KANG\b2000 <hanqjun2660@gmail.com>
2023-09-18 16:57:10 +0900

    라이브러리 버전 변경

--- ex04/src/main/java/org/zerock/controller/ReplyController.java
+++ ex04/src/main/java/org/zerock/controller/ReplyController.java
@@ -2,9 +2,15 @@ package org.zerock.controller;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.zerock.domain.Criteria;
+import org.zerock.domain.ReplyVO;
 import org.zerock.service.ReplyService;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/replies/")
@@ -18,5 +24,24 @@ public class ReplyController {
         this.replyService = replyService;
     }
 
-    
+    @PostMapping(value = "/new", consumes = "application/json", produces = {MediaType.TEXT_PLAIN_VALUE})
+    public ResponseEntity<String> create(@RequestBody ReplyVO vo) {
+
+        log.info("ReplyVO " + vo);
+
+        int insertCount = replyService.register(vo);
+
+        log.info("Reply INSERT COUNT " + insertCount);
+
+        return insertCount == 1 ? new ResponseEntity<>("success", HttpStatus.OK) : new ResponseEntity<>("fail", HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @GetMapping(value = "/pages/{bno}/{page}", produces = { MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_JSON_VALUE })
+    public ResponseEntity<List<ReplyVO>> getList(@PathVariable("bno") Long bno, @PathVariable("page") int page) {
+        log.info("getList...........");
+        Criteria cri = new Criteria(page, 10);
+        log.info(String.valueOf(cri));
+
+        return new ResponseEntity<>(replyService.getList(cri, bno), HttpStatus.OK);
+    }
 }
--- ex04/src/main/java/org/zerock/domain/ReplyVO.java
+++ ex04/src/main/java/org/zerock/domain/ReplyVO.java
@@ -1,10 +1,12 @@
 package org.zerock.domain;
 
 import lombok.Data;
+import lombok.Getter;
 
 import java.util.Date;
 
 @Data
+@Getter
 public class ReplyVO {
 
     private Long rno;
