7a3a96af950bf18be893d39791aa0f9ee75f1f9d
KANG\b2000 <hanqjun2660@gmail.com>
2023-09-18 14:51:19 +0900

    댓글 crud 테스트코드 작성

--- ex04/src/test/java/org/zerock/mapper/ReplyMapperTests.java
+++ ex04/src/test/java/org/zerock/mapper/ReplyMapperTests.java
@@ -7,11 +7,16 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.zerock.domain.ReplyVO;
+
+import java.util.stream.IntStream;
 
 @RunWith(SpringRunner.class)
 @ContextConfiguration(classes = {org.zerock.config.RootConfig.class})
 @Log4j
 public class ReplyMapperTests {
+
+    private Long[] bnoArr = { 233L, 141L, 127L, 126L, 125L, 124L, 123L, 122L, 81L };
 
     @Setter(onMethod_ = @Autowired)
     private ReplyMapper replyMapper;
@@ -19,5 +24,46 @@ public class ReplyMapperTests {
     @Test
     public void testMapper() {
         log.info(replyMapper);
+    }
+
+    @Test
+    public void testCreate() {
+
+        IntStream.rangeClosed(1, 10).forEach(i -> {
+            ReplyVO replyVO = new ReplyVO();
+
+            replyVO.setBno(bnoArr[i % 9]);
+            replyVO.setReply("테스트");
+            replyVO.setReplyer("replayer" + i);
+
+            replyMapper.insert(replyVO);
+        });
+    }
+
+    @Test
+    public void testRead() {
+        Long targetRno = 1L;
+
+        ReplyVO replyVO = replyMapper.read(targetRno);
+
+        log.info(replyVO);
+    }
+
+    @Test
+    public void testDelete() {
+        Long targetRno = 1L;
+        log.info(replyMapper.delete(targetRno));
+    }
+
+    @Test
+    public void testUpdate() {
+        Long targetRno = 2L;
+
+        ReplyVO replyVO = new ReplyVO();
+        replyVO.setReply("업데이트된 내용");
+        replyVO.setRno(targetRno);
+        replyVO.setReplyer("업데이트된 작성자");
+
+        log.info(replyMapper.update(replyVO));
     }
 }
